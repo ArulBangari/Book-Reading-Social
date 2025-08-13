cb53b60573ed09271b0e7f73aea2151da3467f0f
Arul Bangari <arulbangari@gmail.com>
2025-08-12 19:32:25 -0700

    Changed case to uppercase

--- app/search/searchPage.js
+++ app/search/searchPage.js
@@ -7,22 +7,16 @@ import { v4 as uuidv4 } from "uuid";
 import Pagination from "./PaginationBar";
 import { getURL } from "./helpers";
 import { useSearchParams } from "next/navigation";
-// How many items will show on each page of the search
 const searchItems = 5;
 
 export default function SearchPage(props) {
   const searchParams = useSearchParams();
-  // Holds all searchParams info
   const title = searchParams.get("title") || "";
   const author = searchParams.get("author") || "";
   const page = searchParams.get("page") || "";
-  // Data will hold the information I recieve on every query of searchQuery
   const [data, setData] = useState(null);
-  // Holds the error the query might return
   const [error, setError] = useState(null);
 
-  // Every time the query info is changed, re render the search page to update the books
-  // Can't use async function in client side so I'm using it in the useEffect
   useEffect(() => {
     const searchQuery = getURL(title, author, page, searchItems);
     if (!searchQuery) return;
@@ -44,7 +38,6 @@ export default function SearchPage(props) {
     return <h1>Error: {error}</h1>;
   }
 
-  // Return null until data is received
   if (data === null) {
     return;
   }
